dc1191a64705ab2e41393d0bba071ab067c7859e
marisavargas <mvargas2138@ceuandalucia.es>
2024-11-20 20:42:44 +0100

    0000000

---
+++ TEMA3/src/EjeciciosRepaso/Ejercicio10.java
@@ -0,0 +1,16 @@
+package EjeciciosRepaso;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("¿Cuantos alumnos hay?");
+	Integer[]num_alun = new Integer[];
+	System.out.println("¿Cual es el nombre del primer alumno?");
+	String alumno1;
+		
+	}
+
+}
