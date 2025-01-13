d2dd54056f9ca61297fdd0a358e44593bdbe436a
marisavargas <mvargas2138@ceuandalucia.es>
2025-01-13 17:56:13 +0100

    000

--- Tema4/src/Ejercicio06/Ejer6.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio06;
-
-public class Ejer6 {
-
-	public static void main(String[] args) {
-		System.out.println(Calculadora.getSuma(5, 8));
-	}
-
-}
--- Tema4/src/Ejercicio06/Ejer8.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio06;
-
-public class Ejer8 {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
