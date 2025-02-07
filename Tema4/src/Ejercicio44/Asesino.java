c59b045a499dd95b538c14f3caf04060588c5b24
marisavargas <mvargas2138@ceuandalucia.es>
2025-02-07 19:19:02 +0100

    0000

--- Tema4/src/Ejercicio44/Asesino.java
+++ Tema4/src/Ejercicio44/Asesino.java
@@ -1,19 +1,22 @@
 package Ejercicio44;
 
-public class Asesino extends Jugador {
- Integer codigoAse;
- Integer puntoVidaAse;
-public Integer getCodigoAse() {
-	return codigoAse;
+public class Asesino extends Personaje {
+ public static final Integer VIDAS=100;
+ public static final Integer PUNTOS=10;
+ public static final Integer SEGUNDOS=5;
+ 
+ public Asesino() {
+	 
+ }
+public static Integer getVidas() {
+	return VIDAS;
 }
-public void setCodigoAse(Integer codigoAse) {
-	this.codigoAse = codigoAse;
+public static Integer getPuntos() {
+	return PUNTOS;
 }
-public Integer getPuntoVidaAse() {
-	return puntoVidaAse;
+public static Integer getSegundos() {
+	return SEGUNDOS;
 }
-public void setPuntoVidaAse(Integer puntoVidaAse) {
-	this.puntoVidaAse = puntoVidaAse;
-}
+ 
  
 }
