df2e33caf63b3be1bd43b90ab3c944ae4cad5f9d
marisavargas <mvargas2138@ceuandalucia.es>
2025-03-05 13:07:57 +0100

    00000

--- TEMA4.REPASO/src/eje/fijo/movil/Telefono.java
+++ TEMA4.REPASO/src/eje/fijo/movil/Telefono.java
@@ -3,29 +3,27 @@ package eje.fijo.movil;
 public class Telefono {
  protected Integer numero;
  protected Boolean enLlamada;
-/*public Telefono(Integer numero) {
-	super();
-	this.numero = numero;
-	this.enLlamada=false;
-}*/
+
 public Integer consultarNumero () {
 	return numero;
 }
- public void marcar() {
+ public void marcar( int nuevoNumero) {
 	 System.out.println("llamando a "+ numero );
-	 if(numero.equals(numero)){
+	 if(nuevoNumero==numero){
 		 System.out.println("comunicando");
 	 }
 	 else {
 		 System.out.println("en comunicacion ");
 		 this.enLlamada=true;
 	 }
-public Boolean colgar() {
+ }
+public void colgar() {
 	if(enLlamada= true) {
 		System.out.println("comunicacion terminada");
 		enLlamada= false;
 	}else {
-		return enLlamada == true;
+		enLlamada=true;
+		return;
 	}
 	
 
--- TEMA4.REPASO/src/eje/fijo/movil/test2.java
+++ TEMA4.REPASO/src/eje/fijo/movil/test2.java
@@ -1,12 +1,21 @@
 package eje.fijo.movil;
 
+import java.math.BigDecimal;
+
 public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Movil movil= new Movil(654654654);
-Fijo fijo = new Fijo( 954954954);
-
-	}
+Movil movil= new Movil(654654654, new BigDecimal (80) , new BigDecimal (45));
+Fijo fijo = new Fijo( 954954954, "calle ancha numero 2-Sevilla");
+movil.consultarNumero();
+System.out.println(movil.numero);
+fijo.consultarNumero();
+System.out.println(fijo.numero);
+movil.marcar(654654654);
+fijo.marcar(610610610);
+movil.colgar();
+movil.colgar();
+}
 
 }
