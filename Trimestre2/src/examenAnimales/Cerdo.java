c0274be93f3bde6d0d3d3b370bcca4fbf96be2fa
marisavargas <mvargas2138@ceuandalucia.es>
2025-03-16 17:02:40 +0100

    0000

---
+++ Trimestre2/src/examenAnimales/Cerdo.java
@@ -0,0 +1,35 @@
+package examenAnimales;
+
+import java.math.BigDecimal;
+
+public class Cerdo extends Animal {
+ private String raza;
+	public Cerdo(Integer numeroRegstro) {
+		super(numeroRegstro);
+		// TODO Auto-generated constructor stub
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	@Override
+	public BigDecimal getPesoMinVenta() {
+		
+		return new BigDecimal(100);
+	}
+	@Override
+	public BigDecimal getPrecioVentaKg() {
+	
+		return VENTACERDO;
+	}
+	@Override
+	public Boolean sePuedeVender() {
+		if(peso.compareTo(getPesoMinVenta())<0) {
+			return false;
+		}
+				return true ;
+	}
+
+}
