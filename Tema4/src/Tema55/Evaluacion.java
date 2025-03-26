a9e6c97b0926a6521058b35658c778c379f2e539
marisavargas <mvargas2138@ceuandalucia.es>
2025-03-26 12:15:06 +0100

    000

---
+++ Tema4/src/Tema55/Evaluacion.java
@@ -0,0 +1,79 @@
+package Tema55;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Evaluacion {
+
+	private Map<String, BigDecimal> alumnos;
+
+	public Evaluacion() {
+		super();
+		alumnos = new HashMap<>();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Boolean addNota(String dni, BigDecimal nota) {
+
+		for (Map.Entry<String, BigDecimal> entry : alumnos.entrySet()) {
+			String key = entry.getKey();
+			BigDecimal val = entry.getValue();
+			if (key.equals(dni) && val == null) {
+				alumnos.put(dni, nota);
+				return true;
+			}
+
+		}
+		if (!alumnos.containsKey(dni)) {
+			alumnos.put(dni, nota);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean corregirNota(String dni, BigDecimal nota) {
+		if (alumnos.containsKey(dni) && !(alumnos.get(dni) == null)) {
+			alumnos.put(dni, nota);
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+
+	public BigDecimal obtenerNotaAlumno(String dni) {
+		if (alumnos.get(dni) == null) {
+			return BigDecimal.ZERO;
+		} else {
+			return alumnos.get(dni);
+		}
+
+	}
+
+	public BigDecimal obtenerNotaMedia() {
+		BigDecimal sumaNota = new BigDecimal(0);
+		Collection<BigDecimal> notas = alumnos.values();
+		for (BigDecimal nota : notas) {
+			if (!(nota == null)) {
+				sumaNota = sumaNota.add(nota);
+
+			}
+		}
+		return sumaNota.divide(new BigDecimal(alumnos.size()), 2, RoundingMode.HALF_DOWN);
+	}
+
+	public List<BigDecimal> obtenerSuspensos() {
+
+		Collection<BigDecimal> notas = alumnos.values();
+		for (BigDecimal nota : notas) {
+			if (nota.min(5)) {
+
+			}
+		}
+	}
+}
