1659dfdcf165b4761b5b4c5ee298b71256612c1f
marisavargas <mvargas2138@ceuandalucia.es>
2025-03-27 12:46:28 +0100

    000

--- Tema4/src/Tema55/Evaluacion.java
+++ Tema4/src/Tema55/Evaluacion.java
@@ -2,11 +2,14 @@ package Tema55;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Evaluacion {
 
@@ -69,14 +72,57 @@ public class Evaluacion {
 	}
 
 	public List<String> obtenerSuspensos() {
-		List<String> alumnoSuspensos= new ArrayList<>();
+		List<String> alumnoSuspensos = new ArrayList<>();
 		Collection<String> notas = alumnos.keySet();
 		for (String nota : notas) {
-			if (alumnos.get(nota).compareTo(new BigDecimal (5))<0) {
+			if (alumnos.get(nota).compareTo(new BigDecimal(5)) < 0) {
 				alumnoSuspensos.add(nota);
 
 			}
 		}
 		return alumnoSuspensos;
 	}
+
+	public void borrarAprobados() {
+		Set<String> dnis = alumnos.keySet();
+		Iterator<String> it = dnis.iterator();
+		while (it.hasNext()) {
+			String dni = (String) it.next();
+			if (alumnos.get(dni).compareTo(new BigDecimal(5)) < 0) {
+				it.remove();
+			}
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat ft = new DecimalFormat("#.0");
+		String aprobados = "Aprobados:\n";
+		String suspensos = "Suspensos:\n";
+
+		Set<String> dnis = alumnos.keySet();
+		for (String dni : dnis) {
+			String linea = "\t" + dni + " (" + ft.format(alumnos.get(dni)) + ")\n";
+			if (alumnos.get(dni).compareTo(new BigDecimal(5)) < 0) {
+				aprobados += linea;
+			} else {
+				suspensos += linea;
+			}
+		}
+
+		return aprobados + "\n" + suspensos;
+	}
+
+	public Map<String, Integer> word0(String[] strings) {
+
+		Map<String, Integer> mapa = new HashMap<>();
+		for (String str : strings) {
+			if (!mapa.containsKey(str)) {
+				mapa.put(str, str.length());
+			}
+		}
+		return mapa;
+	}
+
 }
---
+++ Tema4/src/Tema55/Text55.java
@@ -0,0 +1,11 @@
+package Tema55;
+
+public class Text55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Evaluacion evau= new Evaluacion("23232323d",(7.2));
+		
+	}
+
+}
